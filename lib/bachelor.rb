8c75721a7f1739bb6a2f460623ac17e585d5b976
github username <github email address>
2017-08-24 22:55:56 -0400

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,54 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data[season].each do |contestant|
+    if contestant["status"] == "Winner"
+      return contestant["name"].split(" ").first
+    end
+  end
+
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |season, contestant|
+    contestant.each do |attribute|
+    if attribute["occupation"] == occupation
+      return attribute["name"]
+    end
+  end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  contestant_count = 0
+  data.each do |season, contestant|
+    contestant.each do |attribute|
+      if attribute["hometown"] == hometown
+        contestant_count += 1
+      end
+    end
+  end
+  return contestant_count
+
 end
 
 def get_occupation(data, hometown)
-  # code here
+  occupation_counter = 0
+  data.each do |season, contestant|
+    contestant.each do |attribute|
+      if attribute["hometown"] == hometown
+        return attribute["occupation"]
+        contestant_count += 1
+        break if contestant_count > 1
+      end
+    end
+  end
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  age_total = 0
+  num_of_contestants = 0
+  data[season].each do |contestant_hash|
+    age_total += (contestant_hash["age"]).to_i
+    num_of_contestants += 1
+  end
+  (age_total / num_of_contestants.to_f).round(0)
 end
